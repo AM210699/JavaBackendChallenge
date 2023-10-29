b960bfc53ce287b764584f8eabf22bd4d59b871d
Ana Maria Jaramillo Aramendiz <anyta2106@hotmail.com>
2023-10-29 13:50:13 -0500

    Session 8 Postwork

--- src/main/java/com/example/javabackendchallengepostwork/model/Person.java
+++ src/main/java/com/example/javabackendchallengepostwork/model/Person.java
@@ -1,11 +1,24 @@
 package com.example.javabackendchallengepostwork.model;
 
-import java.util.Comparator;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Objects;
 
+@Entity
+@Table(name = "PEOPLE")
 public class Person implements Comparable<Person> {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = " Name is a required field.")
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Pattern(regexp = "^(\\d{2,4}[- .]?){2}\\d{4}$", message = " The phone must have a format of ##-####-####")
+    @Column(nullable = false, length = 20)
     private String telephone;
 
 
@@ -15,22 +28,6 @@ public class Person implements Comparable<Person> {
                 "name='" + name + '\'' +
                 ", telephone='" + telephone + '\'' +
                 '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
     }
 
     @Override
@@ -50,5 +47,21 @@ public class Person implements Comparable<Person> {
     @Override
     public int compareTo(Person o) {
         return this.name.compareTo(o.name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
--- src/main/java/com/example/javabackendchallengepostwork/service/AgendaService.java
+++ src/main/java/com/example/javabackendchallengepostwork/service/AgendaService.java
@@ -1,22 +1,24 @@
 package com.example.javabackendchallengepostwork.service;
 
 import com.example.javabackendchallengepostwork.model.Person;
-import com.example.javabackendchallengepostwork.persistence.AgendaMemoryDao;
+import com.example.javabackendchallengepostwork.persistence.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class AgendaService {
 
     private final PhoneValidator phoneValidator;
-    private final AgendaMemoryDao agendaMemoryDao;
+    private final PersonRepository personRepository;
 
     @Autowired
-    public AgendaService(PhoneValidator phoneValidator, AgendaMemoryDao agendaMemoryDao) {
+    public AgendaService(PhoneValidator phoneValidator, PersonRepository personRepository) {
         this.phoneValidator = phoneValidator;
-        this.agendaMemoryDao = agendaMemoryDao;
+        this.personRepository = personRepository;
     }
 
     public Person savePerson(Person person) {
@@ -29,11 +31,10 @@ public class AgendaService {
         person.setTelephone(telephoneNumber);
 
 
-        return agendaMemoryDao.savePerson(person);
+        return personRepository.save(person);
     }
 
-    public Set<Person> getPeople() {
-
-        return agendaMemoryDao.getPeople();
+    public List<Person> getPeople() {
+        return personRepository.findAll(Sort.by("name"));
     }
 }
